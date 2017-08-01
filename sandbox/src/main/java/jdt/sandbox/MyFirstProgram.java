0045df6b0f76cf4f280c3af306a066c32d1df0cc
sgasiewska <s.p.gasiewska@gmail.com>
2017-08-01 19:20:17 +0200

    pole kwadratu i prostokata

--- sandbox/src/main/java/jdt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/jdt/sandbox/MyFirstProgram.java
@@ -3,8 +3,26 @@ package jdt.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Sandra");
 
-    System.out.println("Hello word");
+    double l= 5;
+    System.out.println("Pole kwadratu o boku "+l+ "="+area(l));
 
+    double a=3;
+    double b=4;
+    System.out.println("Pole prostokąta o bokach "+a+ " i "+b+"="+area(a,b));
+
+  }
+  public static void hello(String somebody){
+    System.out.println("Hello,"+somebody+ "!");
+
+  }
+  public static double area (double l){
+    return l*l;
+  }
+  public static double area (double a, double b){
+    return a*b;
   }
 }
