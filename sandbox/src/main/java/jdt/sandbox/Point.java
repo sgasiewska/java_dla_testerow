2126849409b99ffd5b5ceedf699874e1209e03a4
sgasiewska <s.p.gasiewska@gmail.com>
2017-08-02 20:01:28 +0200

    Poprawa zad 2

--- sandbox/src/main/java/jdt/sandbox/Point.java
+++ sandbox/src/main/java/jdt/sandbox/Point.java
@@ -12,7 +12,7 @@ public class Point {
 
   public static double distance(Point p1, Point p2) {
 
-    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x))) + ((p2.y - p1.y) * (p2.y - p1.y));
+    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
 
   }
 
--- sandbox/src/main/java/jdt/sandbox/Zadanie2.java
+++ sandbox/src/main/java/jdt/sandbox/Zadanie2.java
@@ -4,10 +4,10 @@ public class Zadanie2 {
 
   public static void main(String[] args) {
     Point p1 = new Point(1, 2);
-    Point p2 = new Point(3, 4);
-    double d = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x))) + ((p2.y - p1.y) * (p2.y - p1.y));
-    System.out.println("Odległośćzy punktami="+d);
-    System.out.println("Odległośćzy punktami="+Point.distance(p1, p2));
+    Point p2 = new Point(4, 6);
+ //   double d = Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
+  //  System.out.println("Odległośćzy punktami="+d);
+    System.out.println("Odległość między punktami="+Point.distance(p1, p2));
 
   }
 
