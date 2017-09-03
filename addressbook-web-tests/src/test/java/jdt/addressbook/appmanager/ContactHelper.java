a641285617a100658974b939d3e8cb2f37a0d752
sgasiewska <s.p.gasiewska@gmail.com>
2017-09-03 21:03:41 +0200

    Zadanie13

--- addressbook-web-tests/src/test/java/jdt/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/jdt/addressbook/appmanager/ContactHelper.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import java.util.List;
 
-public class ContactHelper extends HelperBase{
+public class ContactHelper extends HelperBase {
 
 
   public ContactHelper(WebDriver wd) {
@@ -24,10 +24,11 @@ public class ContactHelper extends HelperBase{
   public void submitContactCreation() {
     click(By.xpath("//div[@id='content']/form/input[21]"));
   }
+
   public void modifi(ContactData contact) {
     fillContactForm(contact, false);
     submitContactModification();
-    contactCache=null;
+    contactCache = null;
     returnToContactPage();
   }
 
@@ -39,12 +40,12 @@ public class ContactHelper extends HelperBase{
     type(By.name("mobile"), contactData.getMobilePhone());
     type(By.name("work"), contactData.getWorkPhone());
     type(By.name("email"), contactData.getMail());
-    type(By.name("email2"),contactData.getMail2());
-    type(By.name("email3"),contactData.getMail3());
+    type(By.name("email2"), contactData.getMail2());
+    type(By.name("email3"), contactData.getMail3());
 
-    if(creation){
+    if (creation) {
       new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
-    } else{
+    } else {
       Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
   }
@@ -58,6 +59,11 @@ public class ContactHelper extends HelperBase{
 
   }
 
+  public void initContactDetailsById(int id) {
+    wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[td[input[@value='" + id + "']]]/td[7]")).click();
+  }
+
+
   public void deleteSelectedContact() {
     click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
   }
@@ -69,6 +75,7 @@ public class ContactHelper extends HelperBase{
   public void initContactModification(int index) {
     wd.findElements(By.cssSelector("img[title = Edit]")).get(index).click();
   }
+
   public void initContactModificationById(int id) {
 
     wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[td[input[@value='" + id + "']]]/td[8]")).click();
@@ -79,9 +86,9 @@ public class ContactHelper extends HelperBase{
     cells.get(7).findElement(By.tagName("a")).click();
 */
   // wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[td[input[@value='"+ id +"']]]/td[8]")).click();
-    //wd.findElements(By.xpath(String.format("//input[@value='%s']/../../td[8]/a", id))).click();
-    //wd.findElements(By.xpath(String.format("/tr[.//input[@value='%s']]/td[8]/a",id))).click();
-    //wd.findElements(By.cssSelector(String.format("a[href='edit.php?id=%s'}", id))).click();
+  //wd.findElements(By.xpath(String.format("//input[@value='%s']/../../td[8]/a", id))).click();
+  //wd.findElements(By.xpath(String.format("/tr[.//input[@value='%s']]/td[8]/a",id))).click();
+  //wd.findElements(By.cssSelector(String.format("a[href='edit.php?id=%s'}", id))).click();
 
   public void submitContactModification() {
     click(By.xpath("//div[@id='content']/form[1]/input[22]"));
@@ -89,9 +96,9 @@ public class ContactHelper extends HelperBase{
 
   public void create(ContactData contact, boolean creation) {
     initContactCreation();
-    fillContactForm (contact,true);
+    fillContactForm(contact, true);
     submitContactCreation();
-    contactCache=null;
+    contactCache = null;
     returnToContactPage();
   }
 
@@ -99,12 +106,12 @@ public class ContactHelper extends HelperBase{
     selectContactById(contact.getId());
     deleteSelectedContact();
     submitContactDeletion();
-    contactCache=null;
+    contactCache = null;
     returnToContactPage();
   }
 
   private void selectContactById(int id) {
-    wd.findElement(By.cssSelector("input[value='"+id+"']")).click();
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public boolean isThereAContact() {
@@ -115,43 +122,52 @@ public class ContactHelper extends HelperBase{
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  private Contacts contactCache=null;
+  private Contacts contactCache = null;
 
   public Contacts all() {
-    if(contactCache!=null){
+    if (contactCache != null) {
       return new Contacts(contactCache);
     }
     contactCache = new Contacts();
-    List<WebElement>wiersze= wd.findElements(By.xpath("//table[@id='maintable']//tr[@name='entry']"));
-    for (WebElement element:wiersze){
+    List<WebElement> wiersze = wd.findElements(By.xpath("//table[@id='maintable']//tr[@name='entry']"));
+    for (WebElement element : wiersze) {
       List<WebElement> cells = element.findElements(By.tagName("td"));
       String lastname = cells.get(1).getText();
       String firstname = cells.get(2).getText();
-      String allPhones= cells.get(5).getText();
+      String allPhones = cells.get(5).getText();
       String allMails = cells.get(4).getText();
       String address = cells.get(3).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname)
-      .withAllPhones(allPhones).withtAllMails(allMails).withAddress(address));
+              .withAllPhones(allPhones).withtAllMails(allMails).withAddress(address));
     }
     return new Contacts(contactCache);
   }
 
   public ContactData infoFromEditForm(ContactData contact) {
     initContactModificationById(contact.getId());
-    String firstname= wd.findElement(By.name("firstname")).getAttribute("value");
-    String lastname= wd.findElement(By.name("lastname")).getAttribute("value");
-    String home= wd.findElement(By.name("home")).getAttribute("value");
-    String mobile= wd.findElement(By.name("mobile")).getAttribute("value");
-    String work= wd.findElement(By.name("work")).getAttribute("value");
-    String mail=wd.findElement(By.name("email")).getAttribute("value");
-    String mail2=wd.findElement(By.name("email2")).getAttribute("value");
-    String mail3=wd.findElement(By.name("email3")).getAttribute("value");
-    String address=wd.findElement(By.name("address")).getAttribute("value");
+    String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+    String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+    String home = wd.findElement(By.name("home")).getAttribute("value");
+    String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+    String work = wd.findElement(By.name("work")).getAttribute("value");
+    String mail = wd.findElement(By.name("email")).getAttribute("value");
+    String mail2 = wd.findElement(By.name("email2")).getAttribute("value");
+    String mail3 = wd.findElement(By.name("email3")).getAttribute("value");
+    String address = wd.findElement(By.name("address")).getAttribute("value");
     wd.navigate().back();
     return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname)
             .withHomePhone(home).withMobiePhone(mobile).withWorkPhone(work)
             .withMail(mail).withMail2(mail2).withMail3(mail3).withAddress(address);
+
+  }
+
+  public ContactData infoFromDetailsForm(ContactData contact) {
+    initContactDetailsById(contact.getId());
+    List<WebElement> wiersze = wd.findElements(By.xpath("//*[@id=\"content\"]"));
+    String allInformation =wiersze.get(0).getText();
+    wd.navigate().back();
+   return new ContactData().withAllInformations(allInformation);
 
   }
 }
--- addressbook-web-tests/src/test/java/jdt/addressbook/tests/ContactDetailsTests.java
+++ addressbook-web-tests/src/test/java/jdt/addressbook/tests/ContactDetailsTests.java
@@ -1,34 +1,56 @@
 package jdt.addressbook.tests;
 
 import jdt.addressbook.model.ContactData;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactDetailsTests extends TestBase{
+public class ContactDetailsTests extends TestBase {
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().contactPage();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData().withFirstname("name1").withLastname("ln1")
+              .withAddress("address").withEmail("test@test.pl").withGroup("test1")
+              .withHomePhone("+1111").withMobiePhone("(11)999").withWorkPhone("22 22 22")
+              .withMail("test@qq.pl").withMail2("test2@ww.pl").withMail3("test3@ew.pl"), true);
+    }
+  }
 
   @Test
 
-  public void testContactPhones(){
-    app.goTo().contactPage();
-    ContactData contact=app.contact().all().iterator().next();
-    ContactData contactInfoFromEditForm =app.contact().infoFromEditForm(contact);
-
-    assertThat(contact.getAllInformations(), equalTo(mergeInformations(contactInfoFromEditForm)));
-  }
-  private String mergeInformations(ContactData contact) {
-    return Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(),contact.getWorkPhone())
-            .stream().filter((s) ->!s.equals(""))
-            .map(ContactPhoneTests::cleaned)
-            .collect(Collectors.joining("\n"));
-
+  public void testContactPhones() {
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    ContactData contactInfoFromDetailsForm = app.contact().infoFromDetailsForm(contact);
+    assertThat(mergeInformations1(contactInfoFromDetailsForm), equalTo(mergeInformations2(contactInfoFromEditForm)));
   }
 
-  public static String cleaned(String phone){
-    return  phone.replaceAll("\\s","").replaceAll("[-()]","");
+  private String mergeInformations1(ContactData contact) {
+    return Arrays.asList(contact.getAllInformations())
+            .stream().filter((s) -> !s.equals(""))
+            .map(ContactDetailsTests::cleaned)
+            .collect(Collectors.joining(""));
+  }
+
+  private String mergeInformations2(ContactData contact) {
+    return Arrays.asList(contact.getFirstname(), contact.getLastname(), contact.getAddress(),
+            contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(),
+            contact.getMail(), contact.getMail2(), contact.getMail3())
+            .stream().filter((c) -> !c.equals(""))
+            .map(ContactDetailsTests::cleaned).collect(Collectors.joining(""));
+
+  }
+
+  public static String cleaned(String data) {
+    return data.replaceAll("\\s", "").replaceAll("[W:]", "")
+            .replaceAll("[H:]", "").replaceAll("[M:]", "")
+            .replaceAll("emberoftest1", "");
   }
 
 }
