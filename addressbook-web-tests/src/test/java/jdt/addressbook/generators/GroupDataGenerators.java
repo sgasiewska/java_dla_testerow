b940c50068cc865a881be3501a6e6e4916c86d4c
sgasiewska <s.p.gasiewska@gmail.com>
2017-09-04 18:02:17 +0200

    6.3JCommander Praca z param z wiersza poleceń

--- addressbook-web-tests/src/test/java/jdt/addressbook/generators/GroupDataGenerators.java
+++ addressbook-web-tests/src/test/java/jdt/addressbook/generators/GroupDataGenerators.java
@@ -1,6 +1,10 @@
 package jdt.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import jdt.addressbook.model.GroupData;
+import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -10,16 +14,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerators {
+  @Parameter (names="-c", description = "Group count")
+  public int count;
+
+  @Parameter (names="-f", description = "Target file")
+  public String file;
+
   public static void main (String [] args) throws IOException {
-    int count =Integer.parseInt(args [0]);
-    File file = new File(args[1]);
+    GroupDataGenerators generator = new GroupDataGenerators();
+    JCommander jCommander = new JCommander(generator);
+    try{
+      jCommander.parse(args);
+    }catch(ParameterException ex){
 
+        jCommander.usage();
+        return;
+      }
+      generator.run();
+    }
+
+    //int count =Integer.parseInt(args [0]);
+   // File file = new File(args[1]);
+
+
+  private void run() throws IOException {
     List<GroupData> groups =generateGroups(count);
-    save(groups, file);
-
+    save(groups, new File(file));
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups =new ArrayList<GroupData>();
     for(int i =0;i<count;i++){
       groups.add(new GroupData().withName(String.format("test %s",i))
@@ -28,7 +51,7 @@ public class GroupDataGenerators {
     return groups;
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer =new FileWriter(file);
     for (GroupData group :groups){
